badc1f33836b0b8fa42867357558095a38896ff5
ZeroBot Founder:Naoum <78745524+ghost-dev-gr@users.noreply.github.com>
2025-03-23 12:25:21 +0200

    Update lib.sh

--- lib/lib.sh
+++ lib/lib.sh
@@ -52,7 +52,7 @@ export SUPPORTED=false
 export PANEL_DL_URL="https://github.com/ghost-dev-gr/panel/archive/refs/tags/latest.tar.gz"
 export WINGS_DL_BASE_URL="https://github.com/ghost-dev-gr/wings/archive/refs/tags/latest.zip"
 export MARIADB_URL="https://downloads.mariadb.com/MariaDB/mariadb_repo_setup"
-export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/pterodactyl-installer/pterodactyl-installer"}
+export GITHUB_BASE_URL=${GITHUB_BASE_URL:-"https://raw.githubusercontent.com/ghost-dev-gr/pterodactyl-installer"}
 export GITHUB_URL="$GITHUB_BASE_URL/$GITHUB_SOURCE"
 
 # Colors
